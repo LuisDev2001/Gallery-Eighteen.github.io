f22f5c4e4f80844546522851c84d173b2bfe6e7f
LuiDev2001 <luisqrja@gmail.com>
2021-02-08 17:37:07 -0500

    Create my own viewer image with vanilla js

--- js/app.js
+++ js/app.js
@@ -1,9 +1,10 @@
 import { imagesUrl } from "./images.js";
 
 const $galleryWrapper = document.getElementById("js_gallery-wrapper");
+const $btnClose = document.getElementById("js_btn-close-overlay");
 
 // Insertar url images dynamic
-for (const [index, { url, text }] of imagesUrl.entries()) {
+for (const [index, { url, text, state }] of imagesUrl.entries()) {
   $galleryWrapper.innerHTML += `
     <div class="galery-item" style="background-image: url(${url})">
       <input type="hidden" autocomplete="off" value="${url}">
@@ -11,11 +12,12 @@ for (const [index, { url, text }] of imagesUrl.entries()) {
         <p>Foto ${index + 1}</p>
         <span>${text}</span>
       </div>
-      <span class="gallery-heart" id="js_heart-button">
+      <span class="gallery-heart" id="js_heart-button" >
         <i class="far fa-heart" id="js_icon-heart-${index + 1}"></i>
       </span>
     </div>
   `;
+  localStorage.setItem("state", state);
 }
 
 $galleryWrapper.addEventListener("click", (event) => {
@@ -25,5 +27,45 @@ $galleryWrapper.addEventListener("click", (event) => {
   } else if (event.target.className === "fas fa-heart") {
     event.target.className = "far fa-heart";
     event.target.style.color = " rgb(255, 255, 255)";
+  } else if (event.target.className === "galery-item") {
+    openShowImage(event);
   }
 });
+
+const getUrlImage = ($event) => {
+  const URL_IMAGE = $event.target.querySelector("input").value;
+  console.log(URL_IMAGE);
+  return URL_IMAGE;
+};
+
+const openOverlay = () => {
+  const $overlay = document.getElementById("js_overlay");
+  document.body.classList.add("overflow-hidden");
+  $overlay.classList.add("active");
+};
+
+const closeOverlay = () => {
+  const $overlay = document.getElementById("js_overlay");
+  document.body.classList.remove("overflow-hidden");
+  $overlay.classList.remove("active");
+};
+
+const openShowImage = (event) => {
+  const $contentShowImage = document.getElementById("js_show-image");
+  const $image = document.getElementById("js_image-show");
+  $contentShowImage.classList.add("active");
+  openOverlay();
+  $image.setAttribute("src", getUrlImage(event));
+};
+
+const closeShowImage = (event) => {
+  const $contentShowImage = document.getElementById("js_show-image");
+  const $image = document.getElementById("js_image-show");
+  $contentShowImage.classList.remove("active");
+  closeOverlay();
+  setTimeout(() => {
+    $image.setAttribute("src", "");
+  }, 300);
+};
+
+$btnClose.addEventListener("click", closeShowImage);
--- js/images.js
+++ js/images.js
@@ -2,57 +2,71 @@ export const imagesUrl = [
   {
     url: "../images/1.jpg",
     text: "Te amo",
+    state: false,
   },
   {
     url: "../images/2.jpg",
     text: "Te deseo",
+    state: false,
   },
   {
     url: "../images/3.jpg",
     text: "Te amo",
+    state: false,
   },
   {
     url: "../images/4.jpg",
     text: "Te amo",
+    state: false,
   },
   {
     url: "../images/5.jpg",
     text: "Te amo",
+    state: false,
   },
   {
     url: "../images/6.jpg",
     text: "Te amo",
+    state: false,
   },
   {
     url: "../images/7.jpg",
     text: "Te amo",
+    state: false,
   },
   {
     url: "../images/8.jpg",
     text: "Te amo",
+    state: false,
   },
   {
     url: "../images/9.jpg",
     text: "Te amo",
+    state: false,
   },
   {
     url: "../images/10.jpg",
     text: "Te amo",
+    state: false,
   },
   {
     url: "../images/11.jpg",
     text: "Te amo",
+    state: false,
   },
   {
     url: "../images/12.jpg",
     text: "Te amo",
+    state: false,
   },
   {
     url: "../images/13.jpg",
     text: "Te amo",
+    state: false,
   },
   {
     url: "../images/14.jpg",
     text: "Te amo",
+    state: false,
   },
 ];
